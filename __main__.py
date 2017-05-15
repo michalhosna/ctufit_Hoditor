6aae80ad403c98854e49b73534cd42a5981979df
Michal Hošna <michal@michalhosna.com>
2017-05-16 00:18:31 +0200

    Init

---
+++ __main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from tkinter import Tk
+from MainWindows import MainWindow
+
+def main():
+
+    root = Tk()
+    MainWindow(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
