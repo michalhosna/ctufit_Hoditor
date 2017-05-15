dd14c46b80ad39a42365097df04322ae37821c03
Michal Hošna <michal@michalhosna.com>
2017-05-16 00:48:16 +0200

    Added destroyer

--- __main__.py
+++ __main__.py
@@ -4,11 +4,10 @@ from tkinter import Tk
 from MainWindows import MainWindow
 
 def main():
-
     root = Tk()
     MainWindow(root)
     root.mainloop()
-
+    root.destroy()
 
 if __name__ == '__main__':
     main()
