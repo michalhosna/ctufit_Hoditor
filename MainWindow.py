535413646c1be2dc6ce6ae2dc843afb9b4fbd179
Michal Hošna <michal@michalhosna.com>
2017-05-16 14:07:10 +0200

    Added shorcuts

--- MainWindow.py
+++ MainWindow.py
@@ -24,7 +24,7 @@ class MainWindow(Frame):
         self.parent = parent
         self.initUI()
 
-    def initUI(self):
+    def initUI(self, *args):
 
         self.parent.title("Hoditor")
         self.pack(fill=BOTH, expand=1)
@@ -33,25 +33,40 @@ class MainWindow(Frame):
         self.parent.config(menu=menubar)
 
         fileMenu = Menu(menubar)
-        fileMenu.add_command(label="Open", command=self.onOpen)
-        fileMenu.add_command(label="Save", command=self.onSave)
+        fileMenu.add_command(label="Open", command=self.onOpen, accelerator="Ctrl + O")
+        fileMenu.add_command(label="Save", command=self.onSave, accelerator="Ctrl + S")
         fileMenu.add_command(label="Save As", command=self.onSaveAs)
 
         menubar.add_cascade(label="File", menu=fileMenu)
 
         effectsMenu = Menu(self.parent)
-        effectsMenu.add_command(label="Rotate CW", command=self.onRotateCW)
-        effectsMenu.add_command(label="Rotate CCW", command=self.onRotateCCW)
-        effectsMenu.add_command(label="Vertical Flip", command=self.onVerticalFlip)
-        effectsMenu.add_command(label="Horizontal Flip", command=self.onHorizontalFlip)
+        effectsMenu.add_command(label="Rotate CW", command=self.onRotateCW, accelerator="Ctrl + R")
+        effectsMenu.add_command(label="Rotate CCW", command=self.onRotateCCW, accelerator="Ctrl + L")
+        effectsMenu.add_command(label="Vertical Flip", command=self.onVerticalFlip, accelerator="Ctrl + V")
+        effectsMenu.add_command(label="Horizontal Flip", command=self.onHorizontalFlip, accelerator="Ctrl + H")
         effectsMenu.add_separator()
-        effectsMenu.add_command(label="Invert", command=self.onInvert)
-        effectsMenu.add_command(label="Grayscale", command=self.onGrayscale)
-        effectsMenu.add_command(label="Lighten", command=self.onLighten)
-        effectsMenu.add_command(label="Darken", command=self.onDarken)
-        effectsMenu.add_command(label="Find edges", command=self.onFindEdges)
-        effectsMenu.add_command(label="Sharpen", command=self.onSharpen)
-        effectsMenu.add_command(label="Blur", command=self.onBlur)
+        effectsMenu.add_command(label="Invert", command=self.onInvert, accelerator="Ctrl + I")
+        effectsMenu.add_command(label="Grayscale", command=self.onGrayscale, accelerator="Ctrl + G")
+        effectsMenu.add_command(label="Lighten", command=self.onLighten, accelerator="Ctrl + A")
+        effectsMenu.add_command(label="Darken", command=self.onDarken, accelerator="Ctrl + B")
+        effectsMenu.add_command(label="Find edges", command=self.onFindEdges, accelerator="Ctrl + E")
+        effectsMenu.add_command(label="Sharpen", command=self.onSharpen, accelerator="Ctrl + C")
+        effectsMenu.add_command(label="Blur", command=self.onBlur, accelerator="Ctrl + D")
+
+        self.bind_all("<Control-o>", self.onOpen)
+        self.bind_all("<Control-s>", self.onSave)
+
+        self.bind_all("<Control-r>", self.onRotateCW)
+        self.bind_all("<Control-l>", self.onRotateCCW)
+        self.bind_all("<Control-v>", self.onVerticalFlip)
+        self.bind_all("<Control-h>", self.onHorizontalFlip)
+        self.bind_all("<Control-i>", self.onInvert)
+        self.bind_all("<Control-g>", self.onGrayscale)
+        self.bind_all("<Control-a>", self.onLighten)
+        self.bind_all("<Control-b>", self.onDarken)
+        self.bind_all("<Control-e>", self.onFindEdges)
+        self.bind_all("<Control-c>", self.onSharpen)
+        self.bind_all("<Control-d>", self.onBlur)
 
         menubar.add_cascade(label="Effects", menu=effectsMenu)
 
@@ -65,10 +80,10 @@ class MainWindow(Frame):
 
         self.reloadImage()
 
-    def onSave(self):
+    def onSave(self, *args):
         self.ImageProcessor.save()
 
-    def onSaveAs(self):
+    def onSaveAs(self, *args):
         ftypes = [('All files', '*')]
         title = 'Choose the output directory'
         dlg = filedialog.asksaveasfilename(filetypes=ftypes, title=title,
@@ -77,7 +92,7 @@ class MainWindow(Frame):
             return
         self.ImageProcessor.saveAs(dlg)
 
-    def onOpen(self):
+    def onOpen(self, *args):
 
         dlg = filedialog.Open(self)
         fl = dlg.show()
@@ -87,50 +102,50 @@ class MainWindow(Frame):
             self.reloadImage()
 
     @actionCall
-    def onRotateCW(self):
+    def onRotateCW(self, *args):
         self.ImageProcessor.rotateCW()
 
     @actionCall
-    def onRotateCCW(self):
+    def onRotateCCW(self, *args):
         self.ImageProcessor.rotateCCW()
 
     @actionCall
-    def onVerticalFlip(self):
+    def onVerticalFlip(self, *args):
         self.ImageProcessor.verticalFlip()
 
     @actionCall
-    def onHorizontalFlip(self):
+    def onHorizontalFlip(self, *args):
         self.ImageProcessor.horizontalFlip()
 
     @actionCall
-    def onInvert(self):
+    def onInvert(self, *args):
         self.ImageProcessor.invert()
 
     @actionCall
-    def onGrayscale(self):
+    def onGrayscale(self, *args):
         self.ImageProcessor.grayscale()
 
     @actionCall
-    def onLighten(self):
+    def onLighten(self, *args):
         self.ImageProcessor.lighten()
 
     @actionCall
-    def onDarken(self):
+    def onDarken(self, *args):
         self.ImageProcessor.darken()
 
     @actionCall
-    def onFindEdges(self):
+    def onFindEdges(self, *args):
         self.ImageProcessor.findEdges()
 
     @actionCall
-    def onSharpen(self):
+    def onSharpen(self, *args):
         self.ImageProcessor.sharpen()
 
     @actionCall
-    def onBlur(self):
+    def onBlur(self, *args):
         self.ImageProcessor.blur()
 
-    def reloadImage(self):
+    def reloadImage(self, *args):
         self.imageLabel.configure(image=None)
         image = self.ImageProcessor.image
         if image.size[0] > 800 or image.size[1] > 800:
